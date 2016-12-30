b6f31a17b1736353c0f11682280d10df1eeb9236
chibitofu <erinmoony@gmail.com>
2016-12-30 11:15:57 -0800

    Start and stop buttons working.

--- egg_timer/ViewController.swift
+++ egg_timer/ViewController.swift
@@ -15,17 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var timerDisplay: UITextField!
     
     @IBAction func startTimer(_ sender: Any) {
-            }
+        timer.invalidate()
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.processTimer), userInfo: nil, repeats: true)
+    }
     
     @IBAction func stopTimer(_ sender: Any) {
-        
+        timer.invalidate()
     }
 
     @IBAction func resetTimer(_ sender: Any) {
+        timer.invalidate()
         
+        timerDisplay.text = "0"
     }
     
     @IBAction func addSixtySeconds(_ sender: Any) {
+    }
+    
+    func processTimer() {
+        if Int(timerDisplay.text!)! > 0 {
+            timerDisplay.text = String(Int(timerDisplay.text!)! - 1)
+        } else {
+            timer.invalidate()
+        }
     }
     
     override func viewDidLoad() {
